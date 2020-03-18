b9ca6c86b3a3b7b1b3c1a02c82f55b56b6cbfcf2
Oniki Yuki <>
2020-03-18 17:07:32 +0900

    証明書ベースプッシュ用にCertAPNSClientsを追加

--- gaurun/global.go
+++ gaurun/global.go
@@ -13,9 +13,12 @@ var (
 	QueueNotification chan RequestGaurunNotification
 	// Stat for Gaurun
 	StatGaurun StatApp
-	// http client for APNs and GCM/FCM
+	// http client for APNs (token based push)
 	APNSClient APNsClient
-	GCMClient  *gcm.Client
+	// http client for APNs (certificate based push)
+	CertAPNSClients map[NotificationOption]APNsClient = map[NotificationOption]APNsClient{}
+	// http client for GCM/FCM
+	GCMClient *gcm.Client
 	// access and error logger
 	LogAccess *zap.Logger
 	LogError  *zap.Logger
